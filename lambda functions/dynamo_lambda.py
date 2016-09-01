532dd18e10d435abf010ced7283b6dc1664dbafb
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2016-09-01 16:14:59 -0400

    added dynamo-db lambda function

---
+++ lambda functions/dynamo_lambda.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+import datetime
+from math import floor
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+client = boto3.client('dynamodb')
+
+
+def lambda_handler(event, context):
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
+    
+    response = s3.get_object(Bucket=bucket, Key=key)
+    print("CONTENT TYPE: " + response['ContentType'])
+    open_data = response["Body"]
+    print("The file that is being uploaded is: ", key)
+
+    # assinging variables
+    dataSet = json.load(open_data)
+    expt = dataSet["meta"]["experiment"]
+    subject = dataSet["meta"]["subject"]
+    startT = str(dataSet["meta"]["start_time"])
+    stopT = str(dataSet["meta"]["stop_time"])
+    day = str(dataSet["meta"]["day"])
+    fileName = str(key)
+
+    start = str(dateTimeConvertor(startT))
+    stop = str(dateTimeConvertor(stopT))
+
+    print("uploading...")
+    
+    response = client.put_item(
+        TableName=TableName,
+        Item={
+            'file name': {
+                'S': fileName,
+                },
+            'experiment': {
+                'S': expt,
+                },
+            'subject': {
+                'S': subject,
+                },
+            'start time': {
+                'S': start,
+                },
+            'stop time': {
+                'S': stop,
+                },
+            'day': {
+                'N': day,
+                }
+            }
+    )
+
+def dateTimeConvertor(time):
+    start_date = "2000-01-01"
+    dateTime = start_date+" "+time
+    
+    end_date = datetime.datetime.strptime(dateTime, "%Y-%m-%d %H:%M:%S:%f")
+    return end_date
